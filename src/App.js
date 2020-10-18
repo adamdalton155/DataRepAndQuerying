964f1901209977a628ee56c8c8a5c5101a064d4b
Adam Dalton <adamdalton@adams-mbp.home>
2020-10-18 20:25:19 +0100

    Lab2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+import { Content } from './components/content';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+            {/*Navbar on top of page with buttons to display different compoments on the one page*/}
+          <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+              {/* displays home  */}
+              <Nav.Link href="/">Home</Nav.Link>
+              {/* displays Footer component */}
+              <Nav.Link href="/read">Read</Nav.Link> 
+              {/* displays Header component */}
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+          </Navbar>
+
+          <br></br>
+          <Switch>
+            {/*Link that when pressed displays Content, header or footer exactly as it displays on the page*/}
+            <Route path='/' component={Content} exact></Route> 
+            <Route path='/read' component={Footer} exact></Route>
+            <Route path='/create' component={Header} exact></Route>
+          </Switch>
+
+        </div>
+      </Router>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>My footer Component</h2>
+            </div>
+        );
+    }
+}
